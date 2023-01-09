7246a3881ac261f5aa57c6973e888e45ba16ca78
Mike Rachman <mikerachman1@gmail.com>
2023-01-09 13:27:29 -0500

    main lesson content completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,59 @@
+import React, { Component } from "react";
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>  
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { 
+      task: { 
+        text: '',
+        id: uniqid()
+      },
+      tasks: [], 
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task : {
+        text: e.target.value,
+        id: this.state.task.id,
+      }
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { 
+        text: '',
+        id: uniqid()
+      },
+    });
+  };
+  
+  render() {
+    const { task, tasks } = this.state;
+    return (
+      <div>
+        <h1>Tasks</h1>  
+        <form>
+          <label>
+            Add Task: <input
+                        type="text"
+                        value={task.text}
+                        onChange={this.handleChange} />
+          </label>
+          <button onClick={this.onSubmitTask}>Submit</button>
+        </form>
+        <div>
+          <Overview tasks={tasks} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
